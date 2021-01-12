28c111cb5a63fbbde4c6ae498b11413d80a6abe7
Alex <alexnoir03@gmail.com>
2021-01-12 23:05:32 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const fastify = require('fastify')({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' });
+})
+
+// Run the server!
+fastify.listen(3000, function (err, address) {
+  if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+  }
+  fastify.log.info(`server listening on ${address}`);
+})
